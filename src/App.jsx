a2a57967386a5c4286d4a0aff1dd9b9b777139c1
Vishal Yadav <vishalyxo@gmail.com>
2023-10-18 21:38:16 +0530

    Your commit message here

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Navigation from "./components/Navigation/Navigation";
+import ContactHeader from "./components/ContactHeader/ContactHeader"
+import ContactForm from "./components/ContactForm/ContactForm";
+function App() {
+  return (
+    <div>
+      <Navigation />
+    <ContactHeader />
+    <ContactForm />
+    </div>
+    
+  )
+}
+
+export default App;
